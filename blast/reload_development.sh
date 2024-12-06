342130ff6c34f6f54bf3330fea19ac3306573e11
alacheim <a.lacheim@outlook.de>
2024-12-06 15:41:39 +0000

    updated blast docker

--- blast/reload_development.sh
+++ blast/reload_development.sh
@@ -1,4 +1,4 @@
-sudo docker stop blast_docker
-sudo docker remove blast_docker
-sudo docker build --no-cache -t blast_docker .
-sudo docker run --name blast_docker --volume /mnt/databases:/blast/blastdb -p 6001:6001 blast_docker
+sudo docker stop blast
+sudo docker remove blast
+sudo docker build --no-cache -t blast_image .
+sudo docker run --name blast --volume /mnt/databases:/blast/blastdb -p 6001:6001 blast_image

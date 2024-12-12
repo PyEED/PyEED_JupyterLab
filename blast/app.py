7e7b99bcd995daa85e6273dfdf4b752743efdd4a
Niklas Abraham - INFlux <nikseb57@gmail.com>
2024-12-12 14:03:43 +0000

    added blastn added second path in reload_devlopment

--- blast/app.py
+++ blast/app.py
@@ -78,6 +78,49 @@ async def run_blastp(request: Request):
     except subprocess.CalledProcessError as e:
         raise HTTPException(status_code=400, detail=f"Command failed: {e.stderr}")
     
+
+@app.post("/blastn")
+async def run_blastn(request: Request):
+    data = await request.json()
+    
+    print(f" Received request to run blastn with data: {data}")
+    
+    query_filename = f"in.fasta"
+    result_filename = f"out.out"
+    
+    # Clear or create result file
+    open(result_filename, 'w').close()
+    
+    # Create the fasta file from the query string
+    create_fasta_files_from_seq(data['query'], query_filename)
+    
+    try:
+        command = [
+            "blastn",
+            '-query', query_filename,
+            '-db', data['db'],
+            '-evalue', str(data['evalue']),
+            '-outfmt', str(data['outfmt']),
+            '-num_threads', str(data['num_threads']),
+            '-out', result_filename,
+            '-max_target_seqs', str(data['max_target_seqs'])
+        ]
+        
+        result = subprocess.run(
+            command, 
+            capture_output=True, 
+            check=True,
+            text=True
+        )
+        
+        with open(result_filename, 'r') as file:
+            result_data = file.read()
+        
+        return {"result": result_data}
+    
+    except subprocess.CalledProcessError as e:
+        raise HTTPException(status_code=400, detail=f"Command failed: {e.stderr}")
+    
     
 if __name__ == "__main__":
     import uvicorn

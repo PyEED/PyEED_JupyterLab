614ac11245552148d6c0eb008fa8efa1877b4092
alacheim <a.lacheim@outlook.de>
2024-12-12 10:01:13 +0000

    set volume as testing db

--- blast/reload_development.sh
+++ blast/reload_development.sh
@@ -1,4 +1,4 @@
 sudo docker stop blast
 sudo docker remove blast
 sudo docker build --no-cache -t blast_image .
-sudo docker run --name blast --volume /mnt/databases:/blast/blastdb -p 6001:6001 blast_image
+sudo docker run --name blast --volume /home/ala/BA/mydb:/blast/db -p 6001:6001 blast_image
